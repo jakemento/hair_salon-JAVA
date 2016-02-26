99df1495554ab760a44195853449c45c500f0c46
Jake Steinberg <jakesteinberg@gmail.com>
2016-02-26 11:50:56 -0800

    add and pass fluentilenium tests to ensure proper spark routes

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -17,35 +17,70 @@ public class AppTest extends FluentTest {
       return webDriver;
   }
 
-  // @Rule
-  // public DatabaseRule database = new DatabaseRule();
-  //
-  // @ClassRule
-  // public static ServerRule server = new ServerRule();
-  //
-  // @Test
-  // public void rootTest() {
-  //   goTo("http://localhost:4567/");
-  //   assertThat(pageSource()).contains("");
-  // }
-  //
-  // @Test
-  // public void CuisineIsCreatedTest() {
-  //   goTo("http://localhost:4567/");
-  //   click("a", withText("Add a cuisine"));
-  //   fill("#type").with("mexican");
-  //   submit(".btn");
-  //   assertThat(pageSource()).contains("mexican");
-  // }
-  //
-  // @Test
-  // public void CuisineOnNewPageExistsTest() {
-  //   goTo("http://localhost:4567/");
-  //   click("a", withText("Add a cuisine"));
-  //   fill("#type").with("mexican");
-  //   submit(".btn");
-  //   click("a", withText("mexican"));
-  //   assertThat(pageSource()).contains("mexican");
-  // }
+  @Rule
+  public DatabaseRule database = new DatabaseRule();
 
+  @ClassRule
+  public static ServerRule server = new ServerRule();
+
+  @Test
+  public void rootTest() {
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Stylist list:");
+  }
+
+
+  @Test
+  public void StylistIsDisplayedTest() {
+    goTo("http://localhost:4567/");
+    fill("#stylistName").with("Jimmy");
+    submit(".btn");
+    assertThat(pageSource()).contains("Jimmy");
+  }
+
+  // @Test
+  // public void taskIsDisplayedTest() {
+  //   goTo("http://localhost:4567/tasks/new");
+  //   fill("#description").with("Mow the lawn");
+  //   submit(".btn");
+  //   click("a", withText("View tasks"));
+  //   assertThat(pageSource()).contains("Mow the lawn");
+  // }
+  // @Test
+  // public void taskIsDisplayedTest() {
+  //   goTo("http://localhost:4567/tasks/new");
+  //   fill("#description").with("Mow the lawn");
+  //   submit(".btn");
+  //   click("a", withText("View tasks"));
+  //   assertThat(pageSource()).contains("Mow the lawn");
+  // }
+  //
+  // @Test
+  // public void multipleTasksAreDisplayedTest() {
+  //   goTo("http://localhost:4567/tasks/new");
+  //   fill("#description").with("Mow the lawn");
+  //   submit(".btn");
+  //   goTo("http://localhost:4567/tasks/new");
+  //   fill("#description").with("Buy groceries");
+  //   submit(".btn");
+  //   click("a", withText("View tasks"));
+  //   assertThat(pageSource()).contains("Mow the lawn");
+  //   assertThat(pageSource()).contains("Buy groceries");
+  // }
+  //
+  // @Test
+  // public void taskShowPageDisplaysDescription() {
+  //   goTo("http://localhost:4567/tasks/new");
+  //   fill("#description").with("Do the dishes");
+  //   submit(".btn");
+  //   click("a", withText("View tasks"));
+  //   click("a", withText("Do the dishes"));
+  //   assertThat(pageSource()).contains("Do the dishes");
+  // }
+  //
+  // @Test
+  // public void taskNotFoundMessageShown() {
+  //   goTo("http://localhost:4567/tasks/999");
+  //   assertThat(pageSource()).contains("Task not found");
+  // }
 }
